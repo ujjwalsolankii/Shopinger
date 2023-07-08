7bfaa657fd658565ec4ec667f9e8950f8d360fff
ujjwalsolankii <ujjwalsolank2140@gmail.com>
2023-07-09 04:19:24 +0530

    Added my project

---
+++ src/pages/cart/cart.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react'
+import { PRODUCTS} from '../../products'
+import {ShopContext} from '../../context/shop-context'
+
+export const Cart = () => {
+  const {cartItems} = useContext(ShopContext);
+  return (
+    <div className="cart">
+      <div> <h1>Your Cart Items</h1></div>
+      <div className="cartItems">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0){
+            // return <CartItem data ={product} />;
+          }
+        })}
+      </div>
+    </div>
+  )
+}
