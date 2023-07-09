bc07db4d6ea1776b868caaade8e4b75340ee915a
ujjwalsolankii <ujjwalsolank2140@gmail.com>
2023-07-09 20:49:37 +0530

    Carting Files Updated

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,19 +1,34 @@
 import React, { useContext } from 'react'
-import { PRODUCTS} from '../../products'
-import {ShopContext} from '../../context/shop-context'
+import { PRODUCTS } from '../../products'
+import { ShopContext } from '../../context/shop-context'
+import { CartItem } from "./cart-item"
+import "./cart.css"
+import { useNavigate } from 'react-router-dom'
 
 export const Cart = () => {
-  const {cartItems} = useContext(ShopContext);
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
   return (
     <div className="cart">
-      <div> <h1>Your Cart Items</h1></div>
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
       <div className="cartItems">
         {PRODUCTS.map((product) => {
-          if (cartItems[product.id] !== 0){
-            // return <CartItem data ={product} />;
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
           }
         })}
       </div>
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p>SubTotal : {totalAmount}$</p>
+          <button onClick={() => navigate("/")}> Continue Shopping</button>
+          <button>Checkout</button>
+        </div>
+      ) : (<h3>Your Cart is Empty</h3>)}
     </div>
   )
 }
